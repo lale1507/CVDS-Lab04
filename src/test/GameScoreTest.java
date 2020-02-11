802e1f8d6e9a2a3d795a769aa608caae7b369006
PaoCuellar <paola.cuellar@mail.escuelaing.edu.co>
2020-02-11 18:55:13 -0500

    Continuación

--- src/test/GameScoreTest.java
+++ src/test/GameScoreTest.java
@@ -24,10 +24,113 @@ public class GameScoreTest{
     }
 
     @Test
-    public void PowerScoreTest(){
-        PowerScore puntaje = new PowerScore();
+    public void PowerBonusScoreTest(){
+        PowerBonusScore puntaje = new PowerBonusScore();
         int valorCorrecto = puntaje.calculateScore(0,0);
         Assert.assertEquals(0, valorCorrecto);
+    }
+
+    /*
+    *CONDICIONES DE FRONTERA PARA EL ORIGINALSCORE
+    *correctCount < 0 (-1,0,1)
+    *incorrectCount < 0 (-1,0,1)
+    *los casos de 0 se encuentran como los valores iniciales
+    */
+    @Test
+    public void  OriginalScoreTestLI1(){
+        OriginalScore puntaje = new OriginalScore();
+        int valorCorrecto = puntaje.calculateScore(-1,2);
+        Assert.assertEquals(0, valorCorrecto);
+    }
+
+    @Test
+    public void  OriginalScoreTestLI2(){
+        OriginalScore puntaje = new OriginalScore();
+        int valorCorrecto = puntaje.calculateScore(0,-1);
+        Assert.assertEquals(0, valorCorrecto);
+    }
+    
+    @Test
+    public void  OriginalScoreTestLI3(){
+        OriginalScore puntaje = new OriginalScore();
+        int valorCorrecto = puntaje.calculateScore(1,0);
+        Assert.assertEquals(100, valorCorrecto);
+    }
+
+    @Test
+    public void  OriginalScoreTestLI4(){
+        OriginalScore puntaje = new OriginalScore();
+        int valorCorrecto = puntaje.calculateScore(0,1);
+        Assert.assertEquals(90, valorCorrecto);
+    }
+
+    /*
+    *CONDICIONES DE FRONTERA PARA EL BONUSSCORE
+    *correctCount < 0 (-1,0,1)
+    *incorrectCount < 0 (-1,0,1)
+    *los casos de 0 se encuentran como los valores iniciales
+    */
+    @Test
+    public void  BonusScoreTestLI1(){
+         BonusScore puntaje = new  BonusScore();
+        int valorCorrecto = puntaje.calculateScore(-1,2);
+        Assert.assertEquals(0, valorCorrecto);
+    }
+
+    @Test
+    public void  BonusScoreTestLI2(){
+         BonusScore puntaje = new  BonusScore();
+        int valorCorrecto = puntaje.calculateScore(0,-1);
+        Assert.assertEquals(0, valorCorrecto);
+    }
+
+    @Test
+    public void BonusScoreTestLI3(){
+        BonusScore puntaje = new BonusScore();
+        int valorCorrecto = puntaje.calculateScore(1,0);
+        Assert.assertEquals(10, valorCorrecto);
+    }
+
+    @Test
+    public void  BonusScoreTestLI4(){
+        BonusScore puntaje = new BonusScore();
+        int valorCorrecto = puntaje.calculateScore(1,1);
+        Assert.assertEquals(5, valorCorrecto);
+    }
+
+    /*
+    *CONDICIONES DE FRONTERA PARA EL POWERBONUSSCORE
+    *correctCount < 0 (-1,0,1)
+    *incorrectCount < 0 (-1,0,1)
+    *correctCount > 500 (499,500,501)
+    *los casos de 0 se encuentran como los valores iniciales
+    */
+    @Test
+    public void  PowerBonusScoreTestLI1(){
+        PowerBonusScore puntaje = new PowerBonusScore();
+        int valorCorrecto = puntaje.calculateScore(-1,2);
+        Assert.assertEquals(0, valorCorrecto);
+    }
+
+    @Test
+    public void  PowerBonusScoreTestLI2(){
+        PowerBonusScore puntaje = new PowerBonusScore();
+        int valorCorrecto = puntaje.calculateScore(0,-1);
+        Assert.assertEquals(0, valorCorrecto);
+    }
+
+    @Test
+    public void PowerBonusScoreTestLI3(){
+        PowerBonusScore puntaje = new PowerBonusScore();
+        int valorCorrecto = puntaje.calculateScore(1,0);
+        Assert.assertEquals(5, valorCorrecto);
+    }
+
+    @Test
+    public void  PowerBonusScoreTestLI4(){
+        PowerBonusScore puntaje = new PowerBonusScore();
+        int valorCorrecto = puntaje.calculateScore(2,1);
+        Assert.assertEquals(22, valorCorrecto);
     }
 
 }
